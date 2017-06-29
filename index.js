69303a715ed42971b97c0fc3e220070343920c61
David Tschida <dmtschida1@gmail.com>
2017-06-29 15:56:51 -0400

    Updates to make it read timestamps and authors correctly

--- index.js
+++ index.js
@@ -15,16 +15,41 @@ fs.readFile(__dirname + '/archive.html','utf8' ,function(err, html){
 });
 
 function startProcess($) {
-  $("div.diceroll.d20").each(function(i, element) {
-    //get parent to get timestamp.
-    // var parentNode = $(element).parent(".message");
-    // var timestamp = $(parentNode).find(".tstamp"); 
-    // if(!timestamp) {
-    //   $(parentNode).prevUntil(".message .tstamp");
-    // }
+  console.log("timestamp\tperson\tdietype\tdieroll"); 
+  var timestamp; 
+  var person; 
+  $(".message").each(function(messageNum, message) {
+    //console.log("...processing message #" + messageNum); 
+    //not all messages have timestamps. 
+    if($(message).children(".tstamp").length) {
+      timestamp = $(message).find(".tstamp").text(); 
+      //console.log("...found a new timestamp" + timestamp); 
+    }
 
-    $(element).find(".didroll").each(function(ii, rolledDie) {
-      console.log($(rolledDie).text()); 
-    });
+    //not all messages have the person's name
+    if($(message).children(".by").length) {
+      person = $(message).find(".by").text(); 
+      //console.log("...found a new person" + person); 
+    }
+
+    //lets get the d20 rolls in this message, if any. 
+    if($(message).is(".rollresult")) {
+      //console.log("...we can expect rolls here."); 
+
+      //this message has some dicerolls in it. 
+      //lets loop all of the common dice and grab their rolls. 
+      for(var die = 1; die <= 20; die++) {
+        //console.log("...looking for d" + die); 
+        if($(message).find(".diceroll.d" + die).length) {
+          //console.log("...found d" + die + "!"); 
+          var diceroll = $(message).find(".diceroll.d" + die); 
+
+          $(diceroll).each(function(rollNumber, roll) {
+            var result = $(roll).find(".didroll").text();
+            console.log(timestamp + "\t" + person + "\t" + die + "\t" + result); 
+          });
+        }
+      }
+    }
   });
 }
